a44b07bf38bc1cc61e4a27fe86138ab233eafd14
haldamir95 <haldamir.95@gmail.com>
2020-02-13 13:32:02 -0600

    Tarea2, microservicios, cliente y recepcion

---
+++ Tarea2/src/routers/client-router.js
@@ -0,0 +1,21 @@
+import * as express from 'express'
+
+export const routerV1 = express.Router()
+
+class ClientRouter {
+
+  hola(req, res){
+    res.send('te amor richie');
+  } 
+
+  async acceptOrder (req, res) {
+    const { order } = req.body
+
+    console.log(`No. de orden ${order}, recibido por cliente`)
+    res.send({ received: true })
+  }
+}
+
+const clientrouter = new ClientRouter()
+routerV1.post('/accept', clientrouter.acceptOrder)
+routerV1.get('/hola', clientrouter.hola)
