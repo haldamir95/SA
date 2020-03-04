57ee191f0332d6f0ab54f8490e4e466def9fd80f
haldamir95 <haldamir.95@gmail.com>
2020-03-04 01:42:30 -0600

    back to basic

--- Tarea2-3-4-5/src/api.js
+++ Tarea2-3-4-5/src/api.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const serverless = require('serverless-http');
-
+var path = require('path');
 const app = express();
 
 const router = express.Router();
 
 router.get('/', (req, res) =>{
-    res.json({
-        'Nombre': 'Alan Guzman',
-        'Carnet': '201314733'
-    });
+    res.sendfile(path.join(process.cwd(), './src/index.html'));
+});
+
+router.get('/descarga', (req, res) =>{
+   res.download(path.join(process.cwd(), '/dist', 'archive.tar.gz')); //folder en el servidor con archivos
 });
 
 app.use('/.netlify/functions/api',router);
@@ -17,3 +18,30 @@ app.use('/.netlify/functions/api',router);
 
 
 module.exports.handler = serverless(app);
+
+
+
+
+
+
+
+/*
+
+
+var app = require('express')();
+var http = require('http').Server(app);
+var path = require('path');
+const serverless = require('serverless-http');
+app.get('/asdfasd',function(req, res){
+    res.sendfile('index.html');
+    //res.sendFile(path.join(__dirname, '../../dist', 'archive.tar.gz'));
+});
+app.get('/descargasion', function(req, res){
+    res.download(path.join(__dirname, '../../dist', 'archive.tar.gz')); //folder en el servidor con archivos
+});
+http.listen(3000, function(){
+    console.log('escuchando a puerto 3000');
+});
+
+
+*/
